3b7ace97c7fea365b3bdaf7b489487c4ed202dd2
Kartik Singhal <kartiksinghal@gmail.com>
2016-01-15 12:48:19 -0500

    Add function to get URLs for all years

--- amul_topicals.py
+++ amul_topicals.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
-
 import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = 'http://www.amul.com'
+
+def get_year_urls(page):
+    "Gets URLs of all years for which topicals are available"
+    soup = BeautifulSoup(page, 'html.parser')
+    table = soup.find('table')
+    anchors = table.findAll('a')
+    urls = {}
+    for anchor in anchors:
+        if anchor.text != '': # ignore img links
+            urls[anchor.text[-4:]] = anchor['href']
+    return urls
 
 def fetch_url(url):
     "Fetches the page with the given URL and returns the body"
@@ -8,4 +21,5 @@ def fetch_url(url):
     return r.content
 
 if __name__ == "__main__":
-    print fetch_url("http://www.amul.com/m/amul-hits")
+    page = fetch_url(BASE_URL + '/m/amul-hits')
+    year_urls = get_year_urls(page)
